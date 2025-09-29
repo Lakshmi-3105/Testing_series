35c757bdcfca8aee2302fb709fb2179bd5399adc
Lakshmi-3105 <jayagarlapati2002@gmail.com>
2025-09-29 10:22:44 +0530

    Operator

---
+++ src/Basics_Java/Lab02_Operators.java
@@ -0,0 +1,14 @@
+package Basics_Java;
+
+public class Lab02_Operators {
+    public static void main(String[] args) {
+
+        int sal=13;
+        boolean b=!(sal>11||sal<4);
+        System.out.println(b);
+
+
+
+
+    }
+}
